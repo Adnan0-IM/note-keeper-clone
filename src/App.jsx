260ccf1d9ac006c6fccfe8777233e34be8238471
Adnan Iliyasu Muhammad <adnanilyas969@gmail.com>
2024-12-20 00:00:10 +0100

    version 1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Entry from "./Entry"
+import Footer from "./Footer";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+     <Entry />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
